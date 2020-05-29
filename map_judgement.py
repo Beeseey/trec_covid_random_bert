9c5d53031dcf7ff2050615ad5284e4ee46151925
Mahmudur Rahman <bisilayode@yahoo.com>
2020-05-29 14:38:39 -0400

    mapped_judg

--- map_judgement.py
+++ map_judgement.py
@@ -47,11 +47,10 @@ def read_rel_judge(txt_file):
 					doc_idx_rel.append(info_dict['id'])
 
 			data_dict[_query] = {'rel':doc_idx_rel,'not_rel':doc_idx_not_rel,'partial':doc_idx_partial}
+			query_info_list = []
 			
 		query_info_list.append(run_dict)
 		_query = query
-		if query == '2':
-			break
 		
 	return data_dict
 
@@ -59,9 +58,9 @@ data_dict = read_rel_judge('qrels-rnd1.txt')
 
 topic_dict = read_topics('topics-rnd1.xml')
 
-print(data_dict)
+#print(data_dict)
 
-raise('stop')
+#raise('stop')
 
 
 def read_result(txt_file):
@@ -73,7 +72,8 @@ def read_result(txt_file):
 	return text
 
 result = read_result('alam-x.txt')
-#file = open('mapped_result.txt', 'w')
+file = open('mapped_result.txt', 'w')
+count_correct = 0
 count = 0
 
 for line in result.split('\n'):
@@ -86,17 +86,24 @@ for line in result.split('\n'):
 
 	jdg_dict = data_dict[query]
 
+	line[2] = line[2].replace(' ','')
+
 	if line[2] in jdg_dict['rel']:
+		count_correct+=1
 		jdg = 2
 	elif line[2] in jdg_dict['not_rel']:
+		count+=1
 		jdg = 0
 	elif line[2] in jdg_dict['partial']:
+		count_correct+=1
 		jdg = 1
 	else:
 		jdg = 0
+	count+=1
 
-	text = query+line[1]+line[2]+line[3]+line[4]+' '+str(jdg)+'\n'
-	break
-	#file.write(text)
+	text = query+line[1]+' '+line[2]+line[3]+line[4]+' '+str(jdg)+'\n'
+	file.write(text)
 
-#file.close()
+file.close()
+
+print(count,'documents judged',count_correct,'correctly retrieved')
